d3c8b1952e893459d5840af7ef5b06ec8f4c7ab7
Joseph Werle <jwerle@appnexus.com>
2013-03-20 11:22:37 -0400

    choosing sync targets instead of wildcard

--- make/lib/craft/craft.mk
+++ make/lib/craft/craft.mk
@@ -3,7 +3,7 @@
 ##
 export CRAFT 				:= 1
 TOP 								?= $(shell pwd)
-CRAFT_VERSION 			:= 0.0.3
+CRAFT_VERSION 			:= 0.0.4
 CRAFT_TARGETS 			:= `make c__list_targets__`
 CRAFT_TOP 					:= $(TOP)
 CRAFT_WD 						:= $(shell pwd)
